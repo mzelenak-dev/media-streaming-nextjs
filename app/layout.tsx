cda0e5c0a4c75f3941ffc1c906f832ed973fb66e
zedaenious <matt@zelenak.me>
2025-05-04 20:18:29 -0400

    header navbar: fonts, styles, selected by pathname, hover

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,16 @@
-import type { Metadata } from "next";
 import "./globals.css";
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  weight: ['400', '700'],
+  variable: "--font-manrope",
+});
 
 export const metadata: Metadata = {
   title: "Media Streaming App",
-  description: "Media Streaming App using Next.js v15",
+  description: "Media Streaming App using Next.js 15",
 };
 
 export default function RootLayout({
@@ -13,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className='antialiased'>
+      <body className={`${manrope.className} antialiased`}>
         {children}
       </body>
     </html>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,33 @@
+'use client'
+
+import clsx from 'clsx'
 import React from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
-    <div>Navbar</div>
+    <nav className='bg-black-6 border-4 border-black-12 rounded-[12px] p-[10px]'>
+      <ul className='flex flex-row gap-[30px]'>
+        <li className={clsx('header-nav-item', { selected: pathname === '/' })}>
+          <Link href="/">Home</Link>
+        </li>
+        <li className={clsx('header-nav-item', { selected: pathname === '/browse' })}>
+          <Link href="/browse">Movies & Shows</Link>
+        </li>
+        <li className={clsx('header-nav-item', { selected: pathname === '/search' })}>
+          <Link href="/search">Search</Link>
+        </li>
+        <li className={clsx('header-nav-item', { selected: pathname === '/support' })}>
+          <Link href="/support">Support</Link>
+        </li>
+        <li className={clsx('header-nav-item', { selected: pathname === '/subscriptions' })}>
+          <Link href="/subscriptions">Subscriptions</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
